22ff63d5a621cfd6b920d248c0c5a933b3538c88
mati9980 <44467931+mati9980@users.noreply.github.com>
2020-01-19 22:13:31 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const commentRoutes = require("./api/routes/comments");
 const phonesRoutes = require("./api/routes/phones");
 const userRoutes = require("./api/routes/users");
 
-mongoose.connect("mongodb+srv://mati9980:Warhammer40!@cluster0-7eu57.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://mati9980:haslo@cluster0-7eu57.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use("/uploads", express.static("uploads"));
 app.use(morgan("dev"));
